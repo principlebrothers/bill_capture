6aa15f2e8f77df9ec35f0d25327d3fbaadac2e16
principlebrothers <principlesbrothers@yahoo.com>
2025-04-03 15:00:51 +0000

    Add image enlargement feature and reorganize Check component layout for improved readability

--- app/frontend/pages/Checks/Check.jsx
+++ app/frontend/pages/Checks/Check.jsx
@@ -1,42 +1,54 @@
 import { formatDate } from "../../helpers/dateHelper";
+import { useState } from "react";
 
 export default function Check({ check }) {
+  const [isEnlarged, setIsEnlarged] = useState(false);
 
+  const enlarge = () => setIsEnlarged(!isEnlarged);
+  const imageStyle = {
+    width: isEnlarged ? '100%' : '150px',
+    height: 'auto',
+    cursor: 'pointer',
+    transition: 'width 0.3s ease',
+    maxWidth: '100%',
+  };
   return (
-    <div className='flex items-center justify-between gap-4 p-2 border border-gray-300 rounded-lg shadow-sm hover:shadow-md transition-shadow duration-200'>
-      <p className='flex flex-col gap-2'>
-        <span className='text-gray-500'>Date</span>
-        <span className='text-lg font-bold'>
-          {formatDate(check?.created_at)}
-        </span>
-      </p>
-
-      <p className='flex flex-col gap-2'>
-        <span className='text-gray-500'>Invoice No</span>
-        <span className='text-lg font-bold'>
-          {check?.invoice_numbers?.toString()}
-        </span>
-      </p>
-      <p className='flex flex-col gap-2'>
-        <span className='text-gray-500 text-center'>Company</span>
-        <span className='text-lg font-bold max-w-fit text-center'>
-          {' '}
-          {check?.company_name?.toString()}
-        </span>
-      </p>
-      <p className='flex flex-col gap-2 items-center whitespace-nowrap'>
-        <span className='text-gray-500 max-w-fit'>Check No</span>
-        <span className='text-lg font-bold'>
-          {check?.check_number?.toString()}
-        </span>
-      </p>
-
-      <div className='flex items-center justify-center'>
-        <img
-          src={check.image}
-          alt='Check'
-          className='w-24 h-24 object-cover rounded-sm shadow-md'
-        />
+    <div className='flex gap-x-2 p-2 border max-w-fit border-gray-300 rounded-lg shadow-sm hover:shadow-md transition-shadow duration-200'>
+      <img
+        src={check.image}
+        alt='Check'
+        onClick={enlarge}
+        style={imageStyle}
+        className='w-32 h-24 object-cover rounded-sm shadow-md'
+      />
+      <div className={`${isEnlarged ? 'hidden' : ''}`}>
+        <p className='flex flex-wrap gap-x-2'>
+          <span className='text-gray-500 text-center'>Company:</span>
+          <span className='text-lg font-bold max-w-fit text-center'>
+            {' '}
+            {check?.company_name?.toString()}
+          </span>
+        </p>
+        <p className='flex flex-wrap gap-x-2'>
+          <span className='text-gray-500'>Invoice No: </span>
+          <span className='text-lg font-bold'>
+            {check?.invoice_numbers?.toString()}
+          </span>
+        </p>
+        <p className='flex gap-2 items-center whitespace-nowrap'>
+          <span className='text-gray-500 whitespace-nowrap'>
+            Check No:
+          </span>
+          <span className='text-lg font-bold'>
+            {check?.check_number?.toString()}
+          </span>
+        </p>
+        <p className='flex flex-wrap gap-x-2'>
+          <span className='text-gray-500'>Recorded Date:</span>
+          <span className='text-lg font-bold'>
+            {formatDate(check?.created_at)}
+          </span>
+        </p>
       </div>
     </div>
   );
